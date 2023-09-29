e42c10be03e7431fe07f19e968bb58a703f59370
Fernando Ferreira <121515671+fernandof46@users.noreply.github.com>
2023-09-29 15:31:29 -0300

    Adicionar arquivos de projeto.

---
+++ PilhaEstatica/Pilha.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilhaEstatica
+{
+    internal class Pilha
+    {
+        string[] valores;
+        int topo = 0;
+        int limite;
+
+        public Pilha(int Tamanho) // construtor >> setup
+        { 
+            valores = new string[Tamanho];
+            limite = Tamanho;
+        }
+
+        public void Empilhar(string valor)
+        {
+            if (topo < limite)
+            {
+                valores[topo] = valor;
+                topo++;
+            }
+            else
+            {
+                Console.WriteLine("Pilha cheia");
+            }
+        }
+
+        public void Desempilhar()
+        {
+            if (topo > 0)
+            {
+                //valores[topo - 1] = "";
+                topo--;
+            }
+            else
+            {
+                Console.WriteLine("Pilha Vazia!");
+            }
+        }
+
+        public void Imprimir()
+        {
+            if (topo > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Impressão da Pilha");
+
+                for (int i = (topo - 1); i >= 0; i--)
+                {
+                    Console.WriteLine(valores[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Pilha vazia!");
+            }
+        }
+
+    }
+}
---
+++ PilhaEstatica/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using PilhaEstatica;
+
+Console.WriteLine("Pilha Estática");
+Console.WriteLine();
+Console.WriteLine();
+
+// consumindo a classe Pilha
+Pilha pilha = new Pilha(5);
+
+// empilhandos valores na Pilha
+pilha.Empilhar("A");
+pilha.Empilhar("B");
+pilha.Empilhar("C");
+pilha.Empilhar("D");
+pilha.Empilhar("E");
+pilha.Empilhar("F"); // pilha cheia
+
+// desempilhando valores da Pilha
+pilha.Desempilhar();
+pilha.Desempilhar();
+//pilha.Desempilhar();
+//pilha.Desempilhar();
+//pilha.Desempilhar();
+//pilha.Desempilhar(); // pilha vazia
+
+pilha.Imprimir();
+
